28e2b8ba1cbff456cefe0ba3ec9303a075a11d70
lrpineda <luicks212@gmail.com>
2022-08-16 00:35:44 -0600

    adding gql queries

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+// import ApoloServer 
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 4000;
+// create a new ApolloServer and pass in our schema
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Create a new instance of an Apollo server with the GraphQL schema.
+const startApolloServer = async (tyeDefs, resolvers) => {
+  await server.start();
+  // integrate the Apollo server with our express app
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+      app.listen(PORT, () => {
+          console.log(`Server is running on port ${PORT}`);
+          // log where we can go test out GQL API
+          console.log(`Go to http://localhost:${PORT}${server.graphqlPath} to run queries!`);
+      });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
