08675324f104bc4fc5084c2242b2863c0c7d50ba
lrpineda <luicks212@gmail.com>
2022-08-16 01:58:18 -0600

    adding mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,10 @@ const { gql } = require('apollo-server-express');
 
 // create out tyeDefs
 const typeDefs = gql`
+  type Auth {
+    token: ID!
+    user: User
+  }
   type User {
     _id: ID
     username: String
@@ -28,14 +32,24 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     thoughts(username: String): [Thought]
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
